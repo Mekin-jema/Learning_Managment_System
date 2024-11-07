6247fe3119808d0d0d1a3df36ed68bec46113e3f
Mekin Jemal <mekinjemal999@gmail.com>
2024-11-07 21:27:09 +1100

    complete connection of redis database

---
+++ Backend/src/db/redisDatabase.ts
@@ -0,0 +1,12 @@
+import { Redis } from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
+
+const redisClient = () => {
+  if (process.env.REDIS_URL) {
+    return process.env.REDIS_URL;
+  }
+  throw new Error("Redis URL is not defined");
+};
+
+export const redis = new Redis(redisClient());
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,12 +1,36 @@
-import express from "express";
-import { Request, Response } from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Request, Response, NextFunction } from "express";
+import connectDB from "./db/mongoDatabase"; //.ts extension is not required
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
+const PORT = process.env.PORT;
+
+//middleware
+app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.get("/", (req, res, next) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running  on port 3000");
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running  on port ${PORT}`);
+  // res.status(200).json({ message: "Server is running" });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Routes${req.originalUrl} not found`) as any;
+  err.status = 404;
+  next(err);
 });
